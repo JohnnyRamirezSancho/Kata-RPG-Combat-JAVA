173e4b572f014a268a3f7a5846a04261ff2aaab3
JohnnyRamirezSancho <info@johnnyramirez.es>
2023-01-10 11:09:00 +0100

    Test have damage

--- src/main/java/com/factoriaf5/kata/Character.java
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -1,5 +1,81 @@
 package com.factoriaf5.kata;
 
 public class Character {
+    int initHealth = 1000;
+    int actualHealth = initHealth;
+    int initDamage = 100;
+    int actualDamage = 100;
+    int initSanation = 100;
+    int actualSanation = 100;
+    int initLevel = 1;
+    int actualLevel = initLevel;
+    boolean alive = true;
+
+    public Character() {
+        this.initHealth = getInitHealth();
+        this.actualHealth = getActualHealth();
+        this.initDamage = getInitDamage();
+        this.actualDamage = getActualDamage();
+        this.initSanation = getInitSanation();
+        this.actualSanation = getActualSanation();
+        this.initLevel = getInitLevel();
+        this.actualLevel = getActualLevel();
+        this.alive = isAlive();
+    }
+
+    public int getInitHealth() {
+        return initHealth;
+    }
+
+    public void setInitHealth(int health) {
+        this.initHealth = health;
+    }
+
+    public int getInitDamage() {
+        return initDamage;
+    }
+
+    public void setInitDamage(int damage) {
+        this.initDamage = damage;
+    }
+
+    public int getLevel() {
+        return initLevel;
+    }
+
+    public void setLevel(int level) {
+        this.initLevel = level;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+    
+    public void setDamage(Character character) {
+        character.actualHealth -= this.initDamage;
+        setHealth(character.actualHealth);
+        if(character.getHealth() <= 0){
+            character.setAlive(false);
+        }
+    }
+
+    public void setSanation(Character character) {
+        if(!character.isAlive()){          
+            return;
+        }
+        character.health += this.sanation;
+        if(character.health > 1000){
+            character.health = 1000;
+
+        }
+        setHealth(character.health);
+        if(character.getHealth() <= 0){
+            character.setAlive(false);
+        }
+    }
     
 }
--- src/test/java/com/factoriaf5/kata/CharacterTest.java
+++ src/test/java/com/factoriaf5/kata/CharacterTest.java
@@ -1,14 +1,37 @@
 package com.factoriaf5.kata;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class CharacterTest {
     
     @Test
-    public void ExampleTest() {
-        assertEquals(2, 2);
+    public void character_have_1000_health_level_1_andis_alive () {
+        Character myCharacter = new Character();
+        assertEquals(myCharacter.alive, true);
+        assertEquals(myCharacter.health, 1000);
+        assertEquals(myCharacter.level, 1);
     }
 
+    @Test
+    public void character_hava_damage () {
+        Character myCharacter = new Character();
+        Character otherCharacter = new Character();
+        int initHealth = otherCharacter.getHealth();
+        myCharacter.setDamage(otherCharacter);
+        myCharacter.setDamage(otherCharacter);
+        myCharacter.setDamage(otherCharacter);
+        myCharacter.setDamage(otherCharacter);
+        myCharacter.setDamage(otherCharacter);
+        myCharacter.setDamage(otherCharacter);
+        myCharacter.setDamage(otherCharacter);
+        myCharacter.setDamage(otherCharacter);
+        myCharacter.setDamage(otherCharacter);
+        myCharacter.setDamage(otherCharacter);
+        assertFalse(otherCharacter.isAlive());
+        assertTrue(otherCharacter.getHealth() < initHealth);
+    }
 }
